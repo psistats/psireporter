f29959c12007ea5082ef6f2eea6a742111450512
alex-dow <adow@psikon.com>
2018-04-18 17:41:12 -0400

    Some further tweaks

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='psireporter',
-      version='0.1.0-develop',
+      version='0.1.0',
       description='Simple reporting microframework',
       author='Alex Dow',
       author_email='adow@psikon.com',
       url='http://github.com/psistats/psireporter',
-      packages=['psireporter']
+      license='MIT',
+      packages=['psireporter'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6'
+    ],
+    python_requires='>=3.5'
 )
